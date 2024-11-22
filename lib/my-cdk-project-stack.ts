d559ddf2ec8327064c5532877ec6a87ecb80a446
VETRI9876 <vvel54881@gmail.com>
2024-11-22 17:31:10 +0530

    fix(sqs-visibility): increased visibility timeout to 600

--- lib/my-cdk-project-stack.ts
+++ lib/my-cdk-project-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Update the visibilityTimeout to 600 seconds
+    const queue = new sqs.Queue(this, 'MyCdkProjectQueue', {
+      visibilityTimeout: cdk.Duration.seconds(600) // Updated value
+    });
   }
 }
